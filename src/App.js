9dfe4a920665517e99878141a3d6fd4d9a3a4d21
galaxy821 <hoshino208@naver.com>
2022-04-07 22:57:54 +0900

    working on news-viewer-app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { useState } from 'react';
+// import axios from '../node_modules/axios/index';
+import axios from 'axios';
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      await axios
+        .get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e93152cbbb9e4d3ebdc13c7b42a34508',
+        )
+        .then((response) => {
+          setData(response.data);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>Load</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
